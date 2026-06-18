8cb1f98c705dca397568c8d65c949c200f432f8c
wj.roh <woojung8405@gmail.com>
2026-06-19 00:02:44 +0900

    complete: leetcode 167

--- src/main/java/leetcode/_167.java
+++ src/main/java/leetcode/_167.java
@@ -4,6 +4,21 @@ package leetcode;
 public class _167 {
 
     public int[] twoSum(int[] numbers, int target) {
+
+        int left = 0;
+        int right = numbers.length - 1;
+
+        while(left < right) {
+
+            int sum = numbers[left] + numbers[right];
+            if (sum == target) {
+                return new int[]{left + 1, right + 1};
+            } else if (sum < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
         return null;
     }
 
