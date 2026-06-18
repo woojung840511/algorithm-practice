876a12abe71f1e63a70ec0fa1ab8bbb7e6cc9ba7
wj.roh <woojung8405@gmail.com>
2026-06-18 13:08:48 +0900

    wip: leetcode 167 setting

---
+++ src/main/java/leetcode/_167.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+public class _167 {
+
+    public int[] twoSum(int[] numbers, int target) {
+        return null;
+    }
+
+}
